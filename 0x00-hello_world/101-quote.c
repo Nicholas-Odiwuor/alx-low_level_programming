83cdfaf616e60188fa925edacead81df2aaa5a1f
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-10-09 19:49:08 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,7 @@
-#include <unistd.h> // Include the necessary header for the write function
+#include <unistd.h>
 
-int main(void) {
-    // The message to be printed to standard error
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-    // Use the write function to print to stderr (file descriptor 2)
-    write(2, message, 59);
-
-    // Return 1 as required
-    return 1;
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
