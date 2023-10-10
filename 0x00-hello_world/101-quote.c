cc2c371ac0bd0d4423018096d9c8d2b3be9bdbf2
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-10-09 19:56:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints the message to the standard error.
+ * Description: This program prints the specified message to the standard error.
+ *
  * Return: Always 1 (error code)
  */
 int main(void)
@@ -11,3 +12,4 @@ int main(void)
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
+
