75ef445ad27804a8a3decc8ca90f77491af22f92
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-10-09 19:52:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the message to the standard error.
+ * Return: Always 1 (error code)
+ */
 int main(void)
 {
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
