08b1e3301ade058c432f5f21487ee95c78bad1e1
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-10-11 07:11:08 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
+void jack_bauer(void);
+/* Add other function prototypes here */
 
 #endif /* MAIN_H */
