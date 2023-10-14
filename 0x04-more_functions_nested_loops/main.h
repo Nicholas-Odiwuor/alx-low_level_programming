791a19eaac9460333ae0e88fff3f47eb5612514f
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-10-13 19:02:28 -0700

    draw diagonal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);  
+int _putchar(char c);  /* Ensure this line is present if not already included */
 
-void print_line(int n);
+void print_diagonal(int n);
 
 #endif /* MAIN_H */
