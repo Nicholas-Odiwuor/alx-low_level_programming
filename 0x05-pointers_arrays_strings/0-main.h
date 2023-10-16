78e483ea88b71fe8ec72ec4a0d94b01f094c8797
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-10-16 06:19:39 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-main.h
@@ -0,0 +1,10 @@
+/* main.h */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for reset_to_98 */
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Thes are pointers, arrays and strings
