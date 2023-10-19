8b9aaa52d03e481cbe96de482744f88e401f62c7
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-10-19 09:18:19 -0700

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _strcmp(char *s1, char *s2);
+int _strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
