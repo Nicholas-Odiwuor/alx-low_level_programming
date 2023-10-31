efbc3e15433a860ec3bcb1c5607e7d68d32e977d
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-10-31 06:13:59 -0700

    Array functions

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: arguments count
+ * @av: arguments vector
+ *
+ * Return: a pointer to a new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+        int i, j, k = 0, len = 0;
+        char *str;
+
+        if (ac == 0 || av == NULL)
+                return (NULL);
+
+        for (i = 0; i < ac; i++)
+        {
+                for (j = 0; av[i][j] != '\0'; j++)
+                {
+                        len++;
+                }
+                len++;
+        }
+
+        str = malloc((len + 1) * sizeof(char));
+        if (str == NULL)
+                return (NULL);
+
+        for (i = 0; i < ac; i++)
+        {
+                for (j = 0; av[i][j] != '\0'; j++)
+                {
+                        str[k] = av[i][j];
+                        k++;
+                }
+                str[k] = '\n';
+                k++;
+        }
+        str[k] = '\0';
+
+        return (str);
+}
