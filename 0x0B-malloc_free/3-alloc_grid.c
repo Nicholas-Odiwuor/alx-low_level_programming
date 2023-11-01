cad09308e6d817ff5797f33fc929209a159c1ca1
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-11-01 03:29:50 -0700

    Array functions

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,28 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
+	int **grid;
+	int i, j;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
-
-    grid = malloc(height * sizeof(int *));
-    if (grid == NULL)
-        return (NULL);
-
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = malloc(width * sizeof(int));
-        if (grid[i] == NULL)
-        {
-            /* free all previously allocated memory and return NULL */
-            for (j = 0; j < i; j++)
-                free(grid[j]);
-            free(grid);
-            return (NULL);
-        }
-        /* initialize all elements to 0 */
-        for (j = 0; j < width; j++)
-            grid[i][j] = 0;
-    }
-
-    return (grid);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL
+				{
+				/* free all previously allocated memory and return NULL */
+				for (j = 0; j < i; j++)
+				free(grid[j]);
+				free(grid)
+				return (NULL);
+				}
+				/* initialize all elements to 0 */
+				for (j = 0; j < width; j++)
+				grid[i][j] = 0;
+				}
+				return (grid);
 }
