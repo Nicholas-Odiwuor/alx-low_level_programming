9fdc4bf9ae22693267cb24fd4d63120372a195a5
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2023-11-17 08:30:59 -0800

    NASM Assembler

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,9 @@
-section .data
-    hello db 'Hello, Holberton', 0
-
-section .text
-    global main
-    extern printf
-
+global   main
+	  extern    printf
 main:
-    ; Prepare the arguments for printf
-    mov rdi, hello  ; The format string
-    xor rax, rax    ; Zero out rax to pass only one argument
-
-    ; Call printf
-    call printf
-
-    ; Return from main
-    mov eax, 0x60   ; The syscall number for exit
-    xor edi, edi    ; Exit code 0
-    syscall         ; Call the kernel
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
