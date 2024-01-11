d49b3dddc1b7da790a42d3607b562c049827083c
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-01-11 06:27:59 -0800

    Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,22 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list
- * @h: Pointer to the head of the list
+ * print_dlistint - prints all the elements of a listintt list
+ * @h: pointer to the start of the linked list
  *
- * Return: The number of nodes in the list
+ * Return: number of node
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t node_count = 0;
+	size_t i;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_count++;
-    }
-
-    return (node_count);
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
-
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,18 @@
+# Doubly Linked List Printer
+
+This C program provides a function to print all elements of a doubly linked list.
+
+## Files
+
+- `lists.h`: Header file containing the structure definition and function prototypes.
+- `0-print_dlistint.c`: Implementation file for the `print_dlistint` function.
+- `main.c`: Example main file for testing the `print_dlistint` function.
+- `README.md`: This file.
+
+## Compilation
+
+Compile the program using the following command:
+
+```bash
+gcc -Wall -Werror -Wextra -pedantic *.c -o your_program_name
+
