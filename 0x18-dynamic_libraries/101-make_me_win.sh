f91ad563a5102ae900be871bb4c57ac6f514c288
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-01-18 21:35:28 -0800

    Dyamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp -q -c https://github.com/Nicholas-Odiwuor/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libs.so
-export LD_PRELOAD=/tmp/libs.so
+wget -P /tmp https://github.com/Nicholas-Odiwuor/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
